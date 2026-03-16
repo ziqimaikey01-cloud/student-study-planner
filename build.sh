38d5e84bb00260674d163112ec23a6cc48f868cc
ziqimaikey01-cloud <ziqimaikey01@gmail.com>
2026-03-16 02:25:04 +0000

    fix build order

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ set -o errexit
 
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
-python manage.py createsuperuser --noinput || true
 python manage.py migrate
+python manage.py createsuperuser --noinput || true
