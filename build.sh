7d8ac58a34964b5dcba13ff826935dd1c834f4ca
ziqimaikey01-cloud <ziqimaikey01@gmail.com>
2026-03-16 02:43:40 +0000

    cleanup build script

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
-python manage.py createsuperuser --noinput || true
+# python manage.py createsuperuser --noinput || true
